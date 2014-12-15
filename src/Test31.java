b21e3cc6877d3721486fdde6ca376c242d32759b
t427795737 <tianof427795737@126.com>
2014-12-15 16:41:24 +0800

    [TestGit3][commit 1]

---
+++ src/Test31.java
@@ -0,0 +1,39 @@
+/**  
+ * @Title:  Test31.java
+ * @Package 
+ * @Description: TODO(用一句话描述该文件做什么)
+ * @author wangxiaoming
+ * @date  Dec 15, 2014 4:40:11 PM
+ * @version V1.0  
+ * Update Logs:
+ * ****************************************************
+ * Name:
+ * Date:
+ * Description:
+ ******************************************************
+ */
+
+/**
+ * @ClassName: Test31
+ * @Description: TODO(这里用一句话描述这个类的作用)
+ * @author wangxiaoming
+ * @date Dec 15, 2014 4:40:11 PM
+ *
+ */
+public class Test31
+{
+
+    /**
+     * @Title: main
+     * @Description: TODO(这里用一句话描述这个方法的作用)
+     * @author wangxiaoming
+     * @param args
+     * @throws
+     */
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+        System.out.println("-------Test31------");
+    }
+
+}
