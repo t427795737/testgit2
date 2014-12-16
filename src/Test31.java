69f1a45b4048fc103a7c2baa45f62644f4ae556d
t427795737 <tianof427795737@126.com>
2014-12-16 13:59:51 +0800

    [testgit2][windows git test1]
    Signed-off-by: t427795737 <tianof427795737@126.com>

--- src/Test31.java
+++ src/Test31.java
@@ -1,7 +1,7 @@
 /**  
  * @Title:  Test31.java
  * @Package 
- * @Description: TODO(用一句话描述该文件做什么)
+ * @Description: TODO(design pattern)
  * @author wangxiaoming
  * @date  Dec 15, 2014 4:40:11 PM
  * @version V1.0  
@@ -15,7 +15,7 @@
 
 /**
  * @ClassName: Test31
- * @Description: TODO(这里用一句话描述这个类的作用)
+ * @Description: design pattern
  * @author wangxiaoming
  * @date Dec 15, 2014 4:40:11 PM
  *
@@ -25,7 +25,7 @@ public class Test31
 
     /**
      * @Title: main
-     * @Description: TODO(这里用一句话描述这个方法的作用)
+     * @Description: design pattern
      * @author wangxiaoming
      * @param args
      * @throws
@@ -37,6 +37,7 @@ public class Test31
         System.out.println("-------Test31------");
         System.out.println("-------Test31------");
         System.out.println("-------Test31------");
+        System.out.println("-------son of bitch------");
     }
 
 }
